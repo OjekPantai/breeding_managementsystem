a1bfced66baac4013d59e8d76c870d6d26576e28
OjekPantai <elvien.purnawan01@gmail.com>
2023-11-20 03:21:05 +0700

    Update add login page and edit profile page

---
+++ app/dashboard/users/[id]/page.jsx
@@ -0,0 +1,46 @@
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+import Image from "next/image";
+
+const SingleUserPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
+        </div>
+        Elvien
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Elvien" />
+          <label>Email</label>
+          <input
+            type="emai"
+            name="email"
+            placeholder="elvien.purnawan01@gmail.com"
+          />
+          <label>Password</label>
+          <input type="password" name="password" />
+          <label>Phone</label>
+          <input type="text" name="phone" placeholder="+6282232328196" />
+          <label>Address</label>
+          <input type="text" name="address" placeholder="Indonesia" />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label> Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
