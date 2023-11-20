dc9204c39629154460aad8119922321014dc55f0
OjekPantai <elvien.purnawan01@gmail.com>
2023-11-21 02:08:08 +0700

    Add Pagination

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -3,24 +3,26 @@
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathName = usePathname();
 
-  const handleSearch = (e) => {
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
 
-    params.set("q", e.target.value);
+    params.set("page", 1);
 
     if (e.target.value) {
-      params.set("q", e.target.value);
+      // Search function optimization (more than 2 words to search)
+      e.target.value.length > 2 && params.set("q", e.target.value);
     } else {
       params.delete("q");
     }
     replace(`${pathName}?${params}`);
-  };
+  }, 300);
 
   return (
     <div className={styles.container}>
