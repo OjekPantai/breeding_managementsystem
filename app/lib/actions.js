1b39dbdabc51f5178fcd04e27e0dc4ac3e92b02f
OjekPantai <elvien.purnawan01@gmail.com>
2023-11-22 06:06:43 +0700

    fix error

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,30 +1,30 @@
-import { revalidatePath } from "next/cache";
-import { connectToDB } from "./utils";
-import { redirect } from "next/navigation";
+// import { revalidatePath } from "next/cache";
+// import { connectToDB } from "./utils";
+// import { redirect } from "next/navigation";
 
-export const addUser = async (formData) => {
-  "use server";
-  const { username, email, password, phone, address, isAdmin, isActive } =
-    Object.fromEntries(formData);
+// export const addUser = async (formData) => {
+//   "use server";
+//   const { username, email, password, phone, address, isAdmin, isActive } =
+//     Object.fromEntries(formData);
 
-  try {
-    connectToDB();
-    const newUser = new User({
-      username,
-      email,
-      password,
-      phone,
-      address,
-      isAdmin,
-      isActive,
-    });
+//   try {
+//     connectToDB();
+//     const newUser = new User({
+//       username,
+//       email,
+//       password,
+//       phone,
+//       address,
+//       isAdmin,
+//       isActive,
+//     });
 
-    await newUser.save();
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to create user!");
-  }
+//     await newUser.save();
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error("Failed to create user!");
+//   }
 
-  revalidatePath("/dashboard/users");
-  redirect("/dashboard/users");
-};
+//   revalidatePath("/dashboard/users");
+//   redirect("/dashboard/users");
+// };
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,19 +1,19 @@
-const Page = () => {
-  const handleForm = async (formData) => {
-    "use server";
-    console.log(formData);
-    const { username } = formData.get("username");
-    console.log("Hello");
-  };
+// const Page = () => {
+//   const handleForm = async (formData) => {
+//     "use server";
+//     console.log(formData);
+//     const { username } = formData.get("username");
+//     console.log("Hello");
+//   };
 
-  return (
-    <div>
-      <form action={handleForm}>
-        <input type="text" name="username" />
-        <button>Send</button>
-      </form>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <form action={handleForm}>
+//         <input type="text" name="username" />
+//         <button>Send</button>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
